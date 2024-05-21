58cd51452a42ec88b5b42f8c53c1add723ed6d5d
parthpandyappp <parthpandyappp@gmail.com>
2024-05-21 17:10:15 +0530

    feat: generate quiz form

---
+++ app/generate-quiz/page.tsx
@@ -0,0 +1,58 @@
+"use client";
+import React, { useState } from "react";
+
+const GenerateQuiz = () => {
+  const [formData, setFormData] = useState({
+    topic: "",
+    description: "",
+    noOfQuestionsToGenerate: 0,
+  });
+
+  const submitHandler = (e: React.FormEvent) => {
+    e.preventDefault();
+  };
+
+  return (
+    <section className="flex min-h-screen flex-col items-center gap-4 p-24">
+      <h1 className="text-2xl font-bold">Generate Quiz</h1>
+
+      <form
+        onSubmit={submitHandler}
+        className="flex flex-col gap-4 w-3/5 justify-center items-center h-32 rounded p-24 mt-6"
+      >
+        <input
+          name="topic"
+          placeholder="Topic"
+          className="w-2/3 border p-2"
+          onChange={(e) => {
+            const { name, value } = e.target;
+            setFormData((prev) => ({ ...prev, [name]: value }));
+          }}
+        />
+        <input
+          name="noOfQuestionsToGenerate"
+          placeholder="No of Questions to Generate"
+          className="w-2/3 border p-2"
+          onChange={(e) => {
+            const { name, value } = e.target;
+            setFormData((prev) => ({ ...prev, [name]: value }));
+          }}
+        />
+        <textarea
+          name="description"
+          placeholder="Description"
+          className="w-2/3 border p-2"
+          onChange={(e) => {
+            const { name, value } = e.target;
+            setFormData((prev) => ({ ...prev, [name]: value }));
+          }}
+        />
+        <button type="submit" className="border px-2 py-1 font-semibold">
+          Generate
+        </button>
+      </form>
+    </section>
+  );
+};
+
+export default GenerateQuiz;
