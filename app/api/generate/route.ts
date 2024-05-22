f5a68184d1755f8bef20e9079e1e66c1d3964259
logan1x <sharmakhushal78@gmail.com>
2024-05-22 19:56:37 +0530

    Fix import path in route.ts

--- app/api/generate/route.ts
+++ app/api/generate/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { generateQuiz, createQuiz } from "./@utils/common";
+import { generateQuiz, createQuiz } from "../@utils/common";
 
 export async function POST(req: Request) {
   try {
