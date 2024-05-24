2704ee2d49a8379e40ab86bc6ec3d8a433c593e8
parthpandyappp <parthpandyappp@gmail.com>
2024-05-25 02:47:48 +0530

    feat: revamped navbar

--- components/nav/nav.tsx
+++ components/nav/nav.tsx
@@ -6,13 +6,11 @@ import { ThemeSupa } from "@supabase/auth-ui-shared";
 import { supabase } from "@/config/supabaseConfig";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
+import Image from "next/image";
 
-type Props = {};
-
-const Nav = (props: Props) => {
-  const { user, onSignOut } = useGetUser();
-
+const Nav: React.FC = () => {
   const router = useRouter();
+  const { user, onSignOut } = useGetUser();
 
   useEffect(() => {
     if (user) {
@@ -26,34 +24,48 @@ const Nav = (props: Props) => {
     <nav className="h-16 border-b-2 flex items-center justify-between container mx-auto px-6 py-1 flex-0">
       <div className="px-4">
         <Link href="/" className="text-xl font-semibold">
-          Quiz Whiz
+          IntelliQuiz
         </Link>
       </div>
       <div>
         {user ? (
-          <div className="gap-2 flex ">
+          <div className="gap-2 flex items-center">
             <Link href="/generate-quiz" className="text-base hover:underline">
               Generate Quiz
             </Link>
             <Link href="/dashboard" className="text-base hover:underline">
               Dashboard
             </Link>
+          </div>
+        ) : null}
+      </div>
+      <div>
+        {user ? (
+          <div className="flex gap-2">
+            <div className="rounded-full h-10 w-10">
+              <Image
+                className="rounded-full object-cover w-full h-full"
+                height={8}
+                width={8}
+                alt="profile"
+                src={user?.avatar_url}
+                unoptimized
+              />
+            </div>
             <button
-              className="text-base hover:underline"
+              className="text-red-400 cursor-pointer font-semibold text-base hover:underline"
               onClick={() => onSignOut()}
             >
               Logout
             </button>
           </div>
         ) : (
-          <>
-            <Auth
-              supabaseClient={supabase}
-              providers={["github"]}
-              appearance={{ theme: ThemeSupa }}
-              onlyThirdPartyProviders
-            />
-          </>
+          <Auth
+            supabaseClient={supabase}
+            providers={["github"]}
+            appearance={{ theme: ThemeSupa }}
+            onlyThirdPartyProviders
+          />
         )}
       </div>
     </nav>
