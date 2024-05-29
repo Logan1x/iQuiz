536c13d8ce9fda07b96501d3c8b21d0294131075
logan1x <sharmakhushal78@gmail.com>
2024-05-29 20:04:43 +0530

    added db types

--- config/supabaseConfig.ts
+++ config/supabaseConfig.ts
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-import { Database } from "@/types/db.types";
+import { Database } from "@/types/db/db.types";
 
 // Create a single supabase client for interacting with your database
 export const supabase = createClient<Database>(
